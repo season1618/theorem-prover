14e415e23b57fe3315ff8780c29dcdf1558922a4
season1618 <seasonminority314@gmail.com>
2025-12-13 23:32:10 +0900

    等号を修正

--- lib/lexer.ml
+++ lib/lexer.ml
@@ -9,16 +9,16 @@ let is_alphabetic c =
   'A' <= c && c <= 'Z' || 'a' <= c && c <= 'z'
 
 let rec tokenize' (s: string) i =
-  if i == String.length s then []
+  if i = String.length s then []
   else
-    match find_opt (fun x -> s.[i] == x) symbols with
+    match find_opt (fun x -> s.[i] = x) symbols with
       Some x -> Delim x :: tokenize' s (i + 1)
     | None ->
         let j = ref(i) in
         while !j < String.length s && is_alphabetic s.[!j] do
           j := !j + 1
         done;
-        if i == !j then raise (TokenError (InvalidToken (sub s i 1)))
+        if i = !j then raise (TokenError (InvalidToken (sub s i 1)))
         else Ident (sub s i (!j - i)) :: tokenize' s !j
 
 let tokenize s = tokenize' s 0
