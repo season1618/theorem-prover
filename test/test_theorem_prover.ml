6b6287b50d6f261df26f88f927e9b45fb3b1d1b8
season1618 <seasonminority314@gmail.com>
2026-01-21 18:01:16 +0900

    テストを修正

--- test/test_theorem_prover.ml
+++ test/test_theorem_prover.ml
@@ -1,35 +1,42 @@
 open Theorem_prover.Type
-open Theorem_prover.Parser
 open Theorem_prover.Lambda
 open Theorem_prover.Value
+open Theorem_prover.Verify
 
 open Format
 
 let () =
-  let (term, _) = parse "%($x:(a).(abc[(a),(*),(@)]))(?y:(b).(def[]))" in
-  printf "%a\n" pp_term term;
-
-  let (term1, _) = parse "%($x:(x).(x))(?x:(x).(x))" in
-  let (term2, _) = parse "%($y:(x).(y))(?z:(x).(z))" in
-  let (term3, _) = parse "%($x:(x).(x))($y:(y).(x))" in
-  let (term4, _) = parse "$x:(*).(x)" in
-  printf "%a [x := y] = %a\n" pp_term term1 pp_term (rename term1 "x" "y");
-  printf "%a [x := y] = %a\n" pp_term term2 pp_term (rename term2 "x" "y");
-  printf "%a [x := a] = %a\n" pp_term term1 pp_term (rename_fresh term1 "x" "a");
-  printf "%a [x := a] = %a\n" pp_term term2 pp_term (rename_fresh term2 "x" "a");
-  printf "%a [x := %a] = %a\n" pp_term term3 pp_term term4 pp_term (subst term3 "x" term4);
-  printf "%a [x := %a] = %a\n" pp_term term3 pp_term term4 pp_term (subst_symb term3 [("x", term4)]);
-  printf "%b\n" (alpha_equiv term1 term2);
-
+  let x : term = Var "x" in
+  let y : term = Var "y" in
+  let z : term = Var "z" in
+  let w : term = Var "w" in
+  let a : term = Var "a" in
+  (* rename *)
+  let term1 : term  = App (Lam ("x", x, x), Pi ("x", x, x)) in
+  let term2 : term  = App (Lam ("y", x, y), Pi ("z", x, z)) in
+  let term3 : term  = App (Lam ("x", y, x), Pi ("x", y, x)) in
+  let term4 : term  = App (Lam ("x", a, x), Pi ("x", a, x)) in
+  assert_alpha_equiv (rename term1 "x" "y") term3;
+  assert_alpha_equiv (rename term2 "x" "y") term3;
+  assert_alpha_equiv (rename term3 "y" "x") term1;
+  assert_alpha_equiv (rename_fresh term1 "x" "a") term4;
+  assert_alpha_equiv (rename_fresh term2 "x" "a") term4;
+  (* substitution *)
+  let term1 : term  = App (Lam ("x", x, App (x, y)), Pi ("y", x, App(x, y))) in
+  let term2 : term  = Lam ("x", x, App (y, z)) in
+  printf "%a [x := %a] =\n" pp_term term1 pp_term term2;
+  printf "  %a\n" pp_term (subst term1 "x" term2);
+  printf "  %a\n" pp_term (subst_symb ["a"; "b"] term1 [("x", term2)]);
+  printf "  %a\n" pp_term (subst_by_eval term1 [("x", term2)]);
+  (* normalization *)
   let defs = [([("B", Type); ("A", Type)], "imply", Some (Pi ("x", Var "A", Var "B")), Type)] in
-  let (term1, _) = parse "%($x:(*).($y:(%($z:(*).(z))(x)).(%(x)(x))))($w:(*).(w))" in
-  let (term2, _) = parse "%($x:(*).(imply[(%($y:(*).(x))(y)),(x)]))($w:(*).(w))" in
+  let term1 : term = App (Lam ("x", Type, Lam ("y", App (Lam ("z", Type, z), x), App (x, x))), Lam ("w", Type, w)) in
+  let term2 : term = App (Lam ("x", Type, Const ("imply", [App (Lam ("y", Type, x), y); x])), Lam ("w", Type, w)) in
   printf "%a ->\n" pp_term term1;
   printf "  %a\n" pp_term (normalize [] term1);
-  printf "  %a\n" pp_term (normalize_symb [] term1);
+  printf "  %a\n" pp_term (normalize_symb ["a"; "b"] [] term1);
   printf "  %a\n" pp_term (normalize_by_eval [] term1);
-
   printf "%a ->\n" pp_term term2;
   printf "  %a\n" pp_term (normalize defs term2);
-  printf "  %a\n" pp_term (normalize_symb defs term2);
+  printf "  %a\n" pp_term (normalize_symb ["a"; "b"] defs term2);
   printf "  %a\n" pp_term (normalize_by_eval defs term2);
